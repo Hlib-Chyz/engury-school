5daae4a4a3734a8bd1ab43e8cb38730d3259c323
GlebChiz <gchyz@jsdaddy.io>
2022-11-24 18:12:03 +0200

    feature: added header

--- components/about-us/about-us.tsx
+++ components/about-us/about-us.tsx
@@ -1,20 +1,16 @@
 import Image from "next/image";
 import styles from "./about-us.module.scss";
+import proKangaroo from "@images/pro-kangaroo.svg";
 
 export default function AboutUs() {
   return (
-    <div className={styles["about-us"]}>
+    <div className={styles.container}>
       <div>
-        <Image
-          src="/images/pro-kangaroo.svg"
-          width="1"
-          height="1"
-          alt="Pro Kangaroo"
-        />
+        <Image src={proKangaroo} width="462" height="450" alt="Pro Kangaroo" />
       </div>
       <section>
         <h3>
-          <span>“Engury”</span> - школа іноземної мови
+          <span>“Engury”</span> - школа <br></br> іноземної мови
         </h3>
         <p>
           Це сучасна онлайн школа вивчення іноземної мови, що допоможе Вам
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,3 +1,30 @@
+import styles from "./header.module.scss";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <h3>ENGORY</h3>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a>Про нас</a>
+          </li>
+          <li>
+            <a>Курси</a>
+          </li>
+          <li>
+            <a>Викладачі</a>
+          </li>
+          <li>
+            <a>Відгуки</a>
+          </li>
+          <li>
+            <a>Контакти</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
