0ac1df58522b01040b2212478339a093237daedc
GlebChiz <gchyz@jsdaddy.io>
2022-12-24 19:02:42 +0200

    feature: added adaptive for form

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,4 +1,5 @@
 import ConsultationRegistrationForm from "@components/consultation-registration-form/consultation-registration-form";
+import FooterForMobile from "@components/footer-for-mobile/footer-for-mobile";
 import Footer from "@components/footer/footer";
 import Header from "@components/header/header";
 import React from "react";
@@ -8,7 +9,8 @@ export default function Form() {
     <div className="container">
       <Header isOrange={true} />
       <ConsultationRegistrationForm />
-      <Footer hasBackground={true} />
+      {/* <Footer hasBackground={true} /> */}
+      <FooterForMobile />
     </div>
   );
 }
